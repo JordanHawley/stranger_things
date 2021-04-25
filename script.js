01a52d571e57f769ce16a1c960ee92afbac87094
Jordan Hawley <mjthriller1993@gmail.com>
2021-04-25 12:10:12 -0700

    User Stays Logged In

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ const BASE_URL =
 
 let currentRunningNumber = 1;
 
+const keepUserLoggedIn = async () => {
+  const me = await fetchMe()
+  const token = localStorage.getItem("token");
+  if (token) {
+    $("#newMessage").css("display", "block");
+    $("#newPost").css("display", "block");
+    $("#myPosts").css("display", "block");
+    $('#logOut').css('display', 'block')
+    $('#loggedInUser').html(me.username)
+    $("#signIn").hide();
+    $("#registerUser").hide();
+  } else {
+    console.log("nothing to hide");
+  }
+};
+
 //FETCHES THE POSTS
 async function fetchPosts() {
   const url = `${BASE_URL}/posts`;
@@ -26,16 +42,18 @@ const fetchAndRender = async () => {
 
 //FETCHES AND RENDERS MY POSTS
 const fetchAndRenderMyPosts = async () => {
-  const me = await fetchMe()
+  const me = await fetchMe();
   // console.log(me)
-  renderMyPostsLinks(me.posts)
-}
+  renderMyPostsLinks(me.posts);
+};
 
 //MODAL THAT DISPLAYS FURTHER INFORMATION ABOUT THE POST
-$("#userposts").on("click", ".list-group-item", function () {
+$("#userposts").on("click", ".list-group-item", async function () {
   $("#postModal").modal("show");
-  let post = $(this).data("post");
-  // console.log(post)
+  const me = await fetchMe();
+  console.log(me);
+  const post = $(this).data("post");
+  console.log(post);
   $(".modal-header").html(
     `<h3>Poster: ${post.author.username}<br><hr><b>${post.title}<b></h3>`
   );
@@ -48,10 +66,15 @@ $("#userposts").on("click", ".list-group-item", function () {
     <br>
     <h6><em><b>Price: ${post.price}<b></em></h6>`
   );
-  // $('.modal-footer').html(
-  //   `${post.}`
-  // )
-  $("#primaryButton").hide();
+  (function checkForLogin() {
+    const token = JSON.parse(localStorage.getItem("token"));
+    if (token && post.author._id !== me._id) {
+      $(".modal-footer").show();
+      $("#primaryButton").text("Send Message");
+    } else {
+      $(".modal-footer").hide();
+    }
+  })();
 });
 
 //MODAL FOR REGISTERING A NEW USER ACCOUNT
@@ -61,7 +84,7 @@ $("#navbarSupportedContent").on("click", "#registerUser", function () {
     '<h3 class="registerUserTitle">Register New User Account</h3>'
   );
   $(".modal-body").html(registerUserModalHTML());
-  $(".modal-footer").hide("#primaryButton");
+  $(".modal-footer").hide();
   // console.log($(".modal-body"));
 });
 
@@ -73,7 +96,7 @@ $("#navbarSupportedContent").on("click", "#signIn", function () {
     '<h3 class="loginUserTitle">Login to your Account</h3>'
   );
   $(".modal-body").html(loginUserModalHTML());
-  $(".modal-footer").hide("#primaryButton");
+  $(".modal-footer").hide();
 });
 
 //MODAL FOR CREATING A NEW POST WHILE LOGGED INTO ACCOUNT
@@ -84,7 +107,7 @@ $("#navbarSupportedContent").on("click", "#newPost", function () {
     '<h3 class="createNewPostTitle">Create A New Post</h3>'
   );
   $(".modal-body").html(createNewPostHTML());
-  $(".modal-footer").hide("#primaryButton");
+  $(".modal-footer").hide();
 });
 
 //FUNCTION THAT LOOPS THROUGH ALL POSTS/RENDERS THEM TO THE SCREEN WHEN INVOKED
@@ -96,47 +119,37 @@ function renderPostsLinks(posts) {
   // console.log(posts); //Posts that have been saved to the database
 }
 
-//GOES THROUGH ALL OF MY POSTS AND RENDERS THEM TO THE MYPOST MODAL
+//GOES THROUGH ALL OF MY POSTS AND RENDERS THEM TO THE "MYPOST" MODAL
 function renderMyPostsLinks(posts) {
   posts.forEach(function (post) {
-    const postElement = createMyPostHTML(post)
-    $('.modal-body').append(postElement)
-  })
+    const postElement = createMyPostHTML(post);
+    $(".modal-body").append(postElement);
+  });
 }
-
-// function renderMyMessages(posts) {
-//   posts.forEach(function (message) {
-
-//   })
-// }
-
-
 
 //FUNCTION THAT OPENS A MODAL TO USERS OWNED POSTS
 $("#navbarSupportedContent").on("click", "#myPosts", function () {
   // console.log("test");
   $("#postModal").modal("show");
-  $(".modal-header").html(
-    '<h3 class="listMyPostsTitle">List of My Posts</h3>'
-  );
-  $(".modal-body").html(
-    fetchAndRenderMyPosts()
-  );
+  $(".modal-header").html('<h3 class="listMyPostsTitle">List of My Posts</h3>');
+  $(".modal-body").html(fetchAndRenderMyPosts());
   $(".modal-footer").hide("#primaryButton");
 });
 
 //WHEN MYPOST IS CLICKED ON IT WILL DISPLAY A LIST OF MESSAGES FOR THAT POST
-$('.modal-body').on('click', '.list-group-item', function() {
-  const post = ($(this).data('myPost'))
-  console.log(post)
-  $('#postModal').modal('show')
+$(".modal-body").on("click", ".list-group-item", function () {
+  const post = $(this).data("myPost");
+  console.log(post);
+  $("#postModal").modal("show");
   $(".modal-header").html(
-    `${post.messages[0] ? `<h3 class="MyPostsMessageTitle">Message for ${post.title}</h3>` : `<h2>There are no Messages</h2>`}`
+    `${
+      post.messages[0]
+        ? `<h3 class="MyPostsMessageTitle">Message for <em>${post.title}</em></h3>`
+        : `<h2>There are no Messages</h2>`
+    }`
   );
-  $(".modal-body").html(
-    `${post.messages[0] ? `<h4>${post.messages[0].fromUser.username}</h4>-<h5>"${post.messages[0].content}"</h5><hr>`: ""}`
-  );
-  $(".modal-footer").show()
+  // $('.modal-body').html(`${post.messages[0].fromUser.username}<hr>${post.messages[0].content}`)
+  $(".modal-footer").show();
   $(".modal-footer").html(`<button id="backButton" type="button" 
         class="btn btn-secondary" data-bs-dismiss="modal">
           Back to Messages
@@ -146,48 +159,48 @@ $('.modal-body').on('click', '.list-group-item', function() {
   Message
 </button>`);
 
-// If post.messages.length > 0 {
-//   let parentContainer = $(`<div>`)
-//  for (let i = 0; i < post.messages.length; i++) {
-//   let currentMesage = post.messages[i]
-// Create a child element that utilizes the data in that message to render whatever you want
-// Then append that element you just created to the parentContainer element 
-//  } 
-// }
-// Append the parentContainer element to whatever modal you want to append it to
-})
-
-
-$('modal-footer').on('click', '#backButton', function() {
+  if (post.messages.length > 0) {
+    let parentContainer = $(`<div class="parentMessages">`);
+    for (let i = 0; i < post.messages.length; i++) {
+      let currentMessage = post.messages[i];
+      // console.log(currentMessage)
+      let childContainer = $(`<div class="childMessages">`);
+      childContainer.append(
+        `<h4>${currentMessage.fromUser.username} : "${currentMessage.content}"`
+      );
+      parentContainer.append(childContainer);
+    }
+    $(".modal-body").html(parentContainer);
+  } else {
+    ("There are no messages");
+  }
+});
+$("modal-footer").on("click", "#backButton", function () {
   $("#postModal").modal("show");
-  $(".modal-header").html(
-    '<h3 class="listMyPostsTitle">List of My Posts</h3>'
-  );
-  $(".modal-body").html(
-    fetchAndRenderMyPosts()
-  );
+  $(".modal-header").html('<h3 class="listMyPostsTitle">List of My Posts</h3>');
+  $(".modal-body").html(fetchAndRenderMyPosts());
   $(".modal-footer").hide("#primaryButton");
-})
+});
 
 //FUNCTION TO FETCH THE DATA FROM ME
 const fetchMe = async () => {
-  const url = `${BASE_URL}/users/me`
-  const token = JSON.parse(localStorage.getItem("token"))
+  const url = `${BASE_URL}/users/me`;
+  const token = JSON.parse(localStorage.getItem("token"));
 
   try {
     const response = await fetch(url, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`
-      }
-    })
-    const { data } = await response.json()
-    return data
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const { data } = await response.json();
+    return data;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 //FUNCTION TO CREATE THE POST HTML BUTTON
 const createPostHTML = (post) => {
@@ -205,7 +218,7 @@ const createMyPostHTML = (post) => {
     post.willDeliver === true ? "YES" : "NO"
   } / Price: ${post.price}
   </h4>`).data("myPost", post);
-}
+};
 
 //FUCNTION TO CREATE THE NEW POST MODAL AND UPDATE THE MODAL IN THE HTML
 const createNewPostHTML = () => {
@@ -226,17 +239,28 @@ const createNewPostHTML = () => {
       <input id="post-price" class="form-control" type="text" required>
     </div>
 
-    <div class="mb-3">
-      <label for="post-willDeliver" class="form-label">Will Deliver</label>
-      <input id="post-willDeliver" class="form-control" type="text" placeholder="Enter 'True' or 'False'" required>
+    <p>Will Deliver?</p>
+
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="flexRadioDefault" id="willDeliverTrue" value="true">
+      <label class="form-check-label" for="willDeliverTrue">
+        YES
+      </label>
+    </div>
+
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="flexRadioDefault" id="willDeliverFalse" value="false">
+     <label class="form-check-label" for="willDeliverFalse">
+        NO
+     </label>
     </div>
 
     <button id="newPostButton" type="submit" class="btn btn-primary">Submit</button>
 
   </form>
 
-</div>`
-}
+</div>`;
+};
 
 //FUCNTION TO CREATE THE REGISTER MODAL AND UPDATE THE MODAL IN THE HTML
 const registerUserModalHTML = () => {
@@ -314,9 +338,9 @@ const registerUser = async (usernameValue, passwordValue) => {
       data: { token },
     } = await response.json();
     console.log(token);
-    localStorage.setItem("token", JSON.stringify(token))
-    $('#loggedInUser').html(usernameValue)
-    hideRegistrationModal()
+    localStorage.setItem("token", JSON.stringify(token));
+    $("#loggedInUser").html(usernameValue);
+    hideRegistrationModal();
   } catch (error) {
     console.error(error);
   }
@@ -333,10 +357,12 @@ const hideRegistrationModal = () => {
   const token = localStorage.getItem("token");
   if (token) {
     $("#postModal").modal("hide");
-    $('#newPost').css('display', "block")
-    $('#myPosts').css('display', "block")
-    $('#signIn').hide()
-    $('#registerUser').hide()
+    $("#newMessage").css("display", "block");
+    $("#newPost").css("display", "block");
+    $("#myPosts").css("display", "block");
+    $('#logOut').css('display', 'block')
+    $("#signIn").hide();
+    $("#registerUser").hide();
   } else {
     console.log("nothing to hide");
   }
@@ -345,30 +371,30 @@ const hideRegistrationModal = () => {
 
 //////////////////////////FUNCTIONS TO LOGIN, SUBMIT LOGIN, HIDE MODAL//////////////////////////
 const loginUser = async (usernameValue, passwordValue) => {
-
   const url = `${BASE_URL}/users/login`;
   try {
-      const response = await fetch(url, { 
-          method: "POST",
-          body: JSON.stringify(
-            {
-            user: { 
-              username: usernameValue,
-              password: passwordValue
-          }
-        }),
-          headers: {
-              "Content-Type": "application/json"
-          }
-      });
-      const { data: {token} } = await response.json();
-      localStorage.setItem("token", JSON.stringify(token))
-      $('#loggedInUser').html(usernameValue)
-      hideLoginModal()
-  } catch(error) {
-      console.error(error);
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        user: {
+          username: usernameValue,
+          password: passwordValue,
+        },
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const {
+      data: { token },
+    } = await response.json();
+    localStorage.setItem("token", JSON.stringify(token));
+    $("#loggedInUser").html(usernameValue);
+    hideLoginModal();
+  } catch (error) {
+    console.error(error);
   }
-} 
+};
 
 $(".modal-body").on("submit", ".login form", (event) => {
   event.preventDefault();
@@ -381,81 +407,103 @@ const hideLoginModal = () => {
   const token = localStorage.getItem("token");
   if (token) {
     $("#postModal").modal("hide");
-    $('#newPost').css('display', "block")
-    $('#myPosts').css('display', "block")
-    $('#signIn').hide()
-    $('#registerUser').hide()
+    $("#newMessage").css("display", "block");
+    $("#newPost").css("display", "block");
+    $("#myPosts").css("display", "block");
+    $('#logOut').css('display', 'block')
+    $("#signIn").hide();
+    $("#registerUser").hide();
   } else {
     console.log("nothing to hide");
   }
 };
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
+////////DELETE A POST THAT BELONGS TO YOU//////////
+
+
+
+
+
+
+/////////////CREATE AND SEND A MESSAGE/////////////
+
+
+
 //////////////////////////FUNCTIONS TO CREATE NEW POST AND SUBMIT//////////////////////////
 const createNewPost = async (requestBody) => {
-  const url = `${BASE_URL}/posts`
-  const token = JSON.parse(localStorage.getItem("token"))
+  const url = `${BASE_URL}/posts`;
+  const token = JSON.parse(localStorage.getItem("token"));
 
   try {
     const response = await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(requestBody),
-    })
-    const result = await response.json()
-    hideNewPostModal()
-    return result
+    });
+    const result = await response.json();
+    hideNewPostModal();
+    return result;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-$('.modal-body').on("submit", ".newPost form", async (event) => {
-  event.preventDefault()
-  const postTitle = $('#post-title').val()
-  const postDescription =$('#post-description').val()
-  const postPrice = $('#post-price').val()
-  const postWillDeliver = $('#post-willDeliver').val()
-  
+$(".modal-body").on("submit", ".newPost form", async (event) => {
+  event.preventDefault();
+  const postTitle = $("#post-title").val();
+  const postDescription = $("#post-description").val();
+  const postPrice = $("#post-price").val();
+  const postWillDeliverTrue = $("#willDeliverTrue").val();
+  const postWillDeliverFalse = $("#willDeliverFalse").val();
+
+  function willIDeliver() {
+    if ($("#willDeliverTrue").prop("checked")) {
+      return postWillDeliverTrue;
+    } else if ($("#willDeliverFalse").prop("checked")) {
+      return postWillDeliverFalse;
+    }
+  }
 
   const requestBody = {
     post: {
       title: postTitle,
       description: postDescription,
       price: postPrice,
-      willDeliver: postWillDeliver.toLowerCase()
-    }
-  }
+      willDeliver: willIDeliver(),
+    },
+  };
   try {
-   await createNewPost(requestBody)
-   fetchAndRender()
+    await createNewPost(requestBody);
+    fetchAndRender();
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
 const hideNewPostModal = () => {
-  $('#postModal').modal('hide')
-}
+  $("#postModal").modal("hide");
+};
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-fetchAndRender()
+fetchAndRender();
+keepUserLoggedIn()
 
 // <p>Will Deliver:</p>
 
- // <div class="form-check">
-    //   <input class="form-check-input" type="radio" name="flexRadioDefault" id="willDeliverTrue" value="True">
-    //   <label class="form-check-label" for="willDeliverTrue">
-    //     True
-    //   </label>
-    // </div>
+// <div class="form-check">
+//   <input class="form-check-input" type="radio" name="flexRadioDefault" id="willDeliverTrue" value="True">
+//   <label class="form-check-label" for="willDeliverTrue">
+//     True
+//   </label>
+// </div>
 
-    // <div class="form-check">
-    //   <input class="form-check-input" type="radio" name="flexRadioDefault" id="willDeliverFalse" value="False">
-    //  <label class="form-check-label" for="willDeliverFalse">
-    //     False
-    //  </label>
-    // </div>
+// <div class="form-check">
+//   <input class="form-check-input" type="radio" name="flexRadioDefault" id="willDeliverFalse" value="False">
+//  <label class="form-check-label" for="willDeliverFalse">
+//     False
+//  </label>
+// </div>
